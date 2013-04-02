493e64e1c591f148cec0a21e0b2550519f7a707b
Dmitry Bobrov <me@dbobrov.com>
2013-04-02 18:49:40 +0400

    Reading without ignore

--- dupme/main.c
+++ dupme/main.c
@@ -1,11 +1,25 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-int k;
-char *buffer;
+#define STDIN 0
+#define STDOUT 1
+
+   
+enum state {
+    NORMAL, IGNORING
+} current_state;
 
 int main(int argc, char** argv)
 {
+    int k;
+    char *buffer;
+    int len;
+    int read_res;
+    int output_start, output_end;
+    int i, j;
+    int write_start;
+    int write_res;
+
     if (argc < 2)
     {
         return 1;
@@ -18,8 +32,43 @@ int main(int argc, char** argv)
         return 2;
     }
 
-    buffer = (char *) malloc(k);
+    k++;
+    buffer = (char *) malloc(k + 1);
+    len = 0;
+    current_state = NORMAL;
 
+    while ((read_res = read(STDIN, buffer + len, k - len)) > 0)
+    {
+        output_start= 0;
+        for (i = len; i < len + read_res; ++i)
+        {
+            if (buf[i] == '\n')
+            {
+               if (current_state == IGNORING)
+               {
+                   output_start = i + 1;
+                   current_state = NORMAL;
+               }
+               output_end = i + 1;
+               for (j = 0; j < 2; ++j)
+               {
+                    write_start = output_start;
+                    while (write_start < output_end)
+                    {
+                        write_res = write(STDOUT, buf + output_start, output_end - output_start);
+                        if (write_res == -1)
+                        {
+                            return 3;
+                        }
+                        write_start += write_res;
+                    }
+               }
+               output_start = i + 1;
+            }
+        }
+        memmove(buf, buf + output_start, len + read_res - output_start + 1);
+        len = len + read_res - output_start + 1;
+    }
     return 0;
 }
 
