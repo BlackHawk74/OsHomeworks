c43dc41eb6a4ae800134a5c1a09f32bfb5d88e45
Dmitry Bobrov <me@dbobrov.com>
2013-04-12 02:16:34 +0400

    Command execution

--- simple-bash/watchthis.c
+++ simple-bash/watchthis.c
@@ -19,31 +19,45 @@ int main(int argc, char** argv)
         puts("Interval must be greater than zero");
         return 2;
     }
-    argv[1] = "-c";
 
     int pipefd[2];
-    pipe(pipefd);
-
-    char buf[100];
+    size_t buf_size = 100;
+    size_t used = 0;
+    char * buf = (char*) malloc(buf_size);
     int read_result;
     while (1) {
+        pipe(pipefd);
         if (fork())
         {
-            while(read_result = read(pipefd[1], buf, sizeof(buf)))
+            close(pipefd[1]);
+            wait(NULL);
+            used = 0;
+            while((read_result = read(pipefd[0], buf + used, buf_size - used)) > 0)
             {
-                int written;
-                for (written = 0; written < read_result; )
+//                write(1, "read\n", 5);
+                used += read_result;
+                if (used == buf_size)
                 {
-                    written += write(STDOUT_FILENO, buf + written, sizeof(buf) - written);
-                } 
+//                    write(1, "realloc\n", 8);
+                    buf_size *= 2;
+                    buf = (char*) realloc((void*) buf, buf_size);
+                }
             }
+
+            int written;
+            for (written = 0; written < used; )
+            {
+                written += write(STDOUT_FILENO, buf + written, used - written);
+            }
+
+            close(pipefd[0]);
             sleep(interval);
 
         } else {
-            dup2(pipefd[0], STDOUT_FILENO);
+            dup2(pipefd[1], 1);
             close(pipefd[0]);
             close(pipefd[1]);
-            execv("/bin/sh", &argv[1]);
+            execvp(argv[2], &argv[2]);
         }
     }
     return 0;
