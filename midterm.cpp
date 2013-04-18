fa9bc904556cc6654dfea0a18b8649fe4db694be
Dmitry Bobrov <me@dbobrov.com>
2013-04-18 20:29:47 +0400

    Running commands

--- midterm.cpp
+++ midterm.cpp
@@ -2,6 +2,7 @@
 #include <cstring>
 #include <sys/types.h>
 #include <fcntl.h>
+#include <sys/wait.h>
 #include <sys/stat.h>
 #include <iostream>
 #include <cstdlib>
@@ -10,20 +11,21 @@
 const int BUFFER_SIZE = 1024;
 const char DELIMITER = ' ';
 
-enum class state_t {
+enum class state_t
+{
     NORMAL, ESCAPED
 };
 
 class reader_t
 {
 public:
-    reader_t(int fd) 
-    : fd(fd),
-    buf_used(0),
-    buf_start(0),
-    eof(false),
-    _has_next(true),
-    buffer((char*)malloc((BUFFER_SIZE + 1) * sizeof(char)))
+    reader_t(int fd)
+        : fd(fd),
+          buf_used(0),
+          buf_start(0),
+          eof(false),
+          _has_next(true),
+          buffer((char *)malloc((BUFFER_SIZE + 1) * sizeof(char)))
     {}
 
 
@@ -31,7 +33,7 @@ public:
     {
         std::string result = "";
         state_t state = state_t::NORMAL;
-        
+
 //        std::cout << "next_argument — eof: " << eof << " buf_start: " << buf_start << " buf_used: " << buf_used << "\n";
         while(true)
         {
@@ -46,19 +48,23 @@ public:
                         result.append(buffer + buf_start, buffer + i);
                         buf_start = i + 1;
                         return result;
-                    } else if (buffer[i] == '\\')
+                    }
+                    else if (buffer[i] == '\\')
                     {
                         result.append(buffer + buf_start, buffer + i);
                         buf_start = i + 1;
                         state = state_t::ESCAPED;
                     }
+
                     break;
+
                 case state_t::ESCAPED:
                     if (buffer[i] != '\\' && buffer[i] != DELIMITER)
                     {
                         std::cerr << "Error in input file" << std::endl;
                         _exit(4);
                     }
+
                     state = state_t::NORMAL;
                     break;
 
@@ -107,18 +113,24 @@ private:
     int do_read()
     {
 //        std::cout << "do_read()\n";
-        if (eof) return 0;
+        if (eof)
+        {
+            return 0;
+        }
+
         if (buf_used == BUFFER_SIZE)
         {
             return -1;
         }
 
         int read_result = read(fd, buffer + buf_used, BUFFER_SIZE - buf_used);
+
         if (read_result < 0)
         {
             std::cerr << "IO error" << std::endl;
             _exit(3);
         }
+
         if (read_result == 0)
         {
             read_result++;
@@ -130,6 +142,117 @@ private:
         return read_result;
     }
 };
+
+const char * TMP_FILE = "/tmp/midterm";
+const char * TMP_OLD = "/tmp/midterm_old";
+
+void run_commands(std::vector<char **> const& commands)
+{
+
+    int prev_pipe = -1;
+    int tmp_fd;
+
+    for (int i = 0; i < commands.size(); i++)
+    {
+        int pipefd[2];
+        pipe(pipefd);
+
+        pid_t pid = fork();
+
+        if (pid)
+        {
+            close(pipefd[1]);
+
+            if (prev_pipe != -1)
+            {
+                close(prev_pipe);
+            }
+
+            prev_pipe = pipefd[0];
+
+            if (i == commands.size() - 1)
+            {
+                tmp_fd = open(TMP_FILE, O_WRONLY);
+                dup2(tmp_fd, pipefd[0]);
+                close(tmp_fd);
+                int status;
+                waitpid(pid, &status, 0);
+
+                if (WIFEXITED(status) && WEXITSTATUS(status) == 0)
+                {
+                    close(pipefd[0]);
+                }
+                else
+                {
+                    _exit(11);
+                }
+            }
+        }
+        else
+        {
+            dup2(pipefd[1], STDOUT_FILENO);
+            close(pipefd[0]);
+            close(pipefd[1]);
+
+            if (prev_pipe != -1)
+            {
+                dup2(prev_pipe, STDIN_FILENO);
+                close(prev_pipe);
+            }
+            else
+            {
+                int in_fd = open(TMP_OLD, O_RDONLY);
+                dup2(in_fd, STDIN_FILENO);
+                close(in_fd);
+            }
+
+            execvp(commands[i][0], commands[i]);
+            _exit(10);
+        }
+    }
+}
+
+void write_all(int fd, char * buf, size_t count)
+{
+    int written;
+
+    // write() has no guarantee to write everything
+    for (written = 0; written < count;)
+    {
+        int t = write(fd, buf + written, count - written);
+
+        if (t == -1) // Write failed
+        {
+            std::cerr << "IO error\n";
+            _exit(4);
+        }
+
+        written += t;
+    }
+}
+
+
+void init()
+{
+    int fd = open(TMP_OLD, O_WRONLY);
+
+    if (fd == -1)
+    {
+        std::cerr << "IO error\n";
+        _exit(12);
+    }
+
+    const int bs = 1024;
+    char * buf = (char *) malloc(bs * sizeof(char));
+    int read_result = 0;
+
+    while((read_result = read(STDIN_FILENO, buf, bs)) > 0)
+    {
+        write_all(fd, buf, read_result);
+    }
+
+    close(fd);
+}
 
 
 int main(int argc, char ** argv)
@@ -148,29 +271,36 @@ int main(int argc, char ** argv)
         return 2;
     }
 
-    std::vector<char**> commands;
+    std::vector<char **> commands;
     reader_t reader(fd);
     std::vector<std::string> current_command;
 
     while (reader.has_next())
     {
         std::string cur = reader.next_argument();
+
         if (cur == "|" || cur == "")
         {
-            char** cmd = (char **) malloc((current_command.size() + 1) * sizeof(char*));
+            char ** cmd = (char **) malloc((current_command.size() + 1) * sizeof(char *));
             cmd[current_command.size()] = NULL;
+
             for (int i = 0; i < current_command.size(); i++)
             {
-                cmd[i] = (char*) malloc((current_command[i].size() + 1) * sizeof(char));
-                memcpy((void*) cmd[i], (const void*) current_command[i].c_str(), sizeof(char) * current_command[i].size());
+                cmd[i] = (char *) malloc((current_command[i].size() + 1) * sizeof(char));
+                memcpy((void *) cmd[i], (const void *) current_command[i].c_str(), sizeof(char) * current_command[i].size());
                 cmd[i][current_command[i].size()] = '\0';
             }
+
             commands.push_back(cmd);
             current_command.clear();
-        } else {
+        }
+        else
+        {
             current_command.push_back(cur);
         }
     }
+
+    init();
 
     for (auto s: commands)
     {
@@ -178,8 +308,11 @@ int main(int argc, char ** argv)
         {
             std::cout << s[i] << " ";
         }
+
         std::cout << "\n--\n";
 
     }
+
+    run_commands(commands);
 }
 
