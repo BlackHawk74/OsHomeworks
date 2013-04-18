4e699cca5360689ce6516bb9f4c1ed2edb5a2a29
Dmitry Bobrov <me@dbobrov.com>
2013-04-18 18:15:17 +0400

    Reader class

--- midterm.cpp
+++ midterm.cpp
@@ -7,6 +7,7 @@
 #include <vector>
 
 const int BUFFER_SIZE = 1024;
+const char DELIMITER = ' ';
 
 enum class state_t {
     NORMAL, ESCAPED
@@ -18,7 +19,9 @@ public:
     reader_t(int fd) 
     : fd(fd),
     buf_used(0),
-    buffer((char*)malloc(BUFFER_SIZE * sizeof(char)))
+    buf_start(0),
+    eof(false),
+    buffer((char*)malloc((BUFFER_SIZE + 1) * sizeof(char)))
     {}
 
 
@@ -28,23 +31,51 @@ public:
         int read_result = 0;
         state_t state = state_t::NORMAL;
         
-        while(1)
+        while(true)
         {
-            read_result = read(fd, buffer + buf_used, BUFFER_SIZE - buf_used);
-
-            if (read_result < 0)
+            for (int i = buf_start; i < buf_used; i++)
             {
-                std::cerr << "IO error" << std::endl;
-                _exit(3);
-            }
-            int end = buf_used + read_result;
+                switch (state)
+                {
+                case state_t::NORMAL:
+                    if (buffer[i] == DELIMITER)
+                    {
+                        result.append(buffer + buf_start, buffer + i);
+                        buf_start = i + 1;
+                        return result;
+                    } else if (buffer[i] == '\')
+                    {
+                        result.append(buffer + buf_start, buffer + i);
+                        buf_start = i + 1;
+                        state = state_t::ESCAPED;
+                    }
+                    break;
+                case state_t::ESCAPED:
+                    if (buffer[i] != '\' && buffer[i] != DELIMITER)
+                    {
+                        std::cerr << "Error in input file" << std::endl;
+                        _exit(4);
+                    }
+                    state = state_t::NORMAL;
+                    break;
 
-            for (int i = buf_used; i < end; ++i)
+                }
+            }
+
+            if (eof)
             {
-
+                break;
             }
+
+            if (do_read() == -1)
+            {
+                result.append(buffer + buf_start, buffer + buf_used);
+                buf_start = 0;
+                buf_used = 0;
+                do_read();
+            }
+
         }
-
         return result;
 
     }
@@ -56,8 +87,35 @@ public:
 
 private:
     int fd;
+    int buf_start;
     int buf_used;
+    bool eof;
     char * buffer;
+
+    int do_read()
+    {
+        if (eof) return 0;
+        if (buf_used == BUFFER_SIZE)
+        {
+            return -1;
+        }
+
+        int read_result = read(fd, buffer + buf_used, BUFFER_SIZE - buf_used);
+        if (read_result < 0)
+        {
+            std::cerr << "IO error" << std::endl;
+            _exit(3);
+        }
+        if (read_result == 0)
+        {
+            read_result++;
+            buffer[buf_used] = DELIMITER;
+            eof = true;
+        }
+
+        buf_used += read_result;
+        return read_result;
+
 };
 
 
@@ -79,6 +137,8 @@ int main(int argc, char ** argv)
 
     std::vector<std::string> command;
     reader_t reader(fd);
+
+    
 
 }
 
