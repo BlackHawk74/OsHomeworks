c2a4775bba5ec24750788fefdc5004ca6574af14
Dmitry Bobrov <me@dbobrov.com>
2013-04-22 17:01:20 +0400

    task2 completed

--- midterm/task2.sh
+++ midterm/task2.sh
@@ -1,3 +1,20 @@
 #!/bin/bash -e
 
+FILE="$1"
+
+prev=""
+
+while read line
+do
+    if [[ $prev =~ ^[0-9]+$ && $line =~ ^[0-9]+$ ]]
+    then
+        sum=$((prev + line))
+        if [ $sum -gt 10 ]; then
+            echo $prev
+            echo $line
+            echo "----"
+        fi
+    fi
+    prev="$line"
+done < "$FILE"
 
