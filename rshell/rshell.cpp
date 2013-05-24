74e322cd489367fa2b34cb83b307d40ca248a41f
Dmitry Bobrov <me@dbobrov.com>
2013-05-24 16:32:31 +0400

    Rshell initial commit

---
+++ rshell/rshell.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <cstdio>
+#include <netdb.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(int, char **)
+{
+    
+}
+
