c80f2fdbb24d0fdc025ccd16ad6875612f2da977
Dmitry Bobrov <me@dbobrov.com>
2013-05-24 18:59:45 +0400

    Simple remote shell

--- rshell/rshell.cpp
+++ rshell/rshell.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <pty.h>
 #include <string>
 #include <cstdlib>
 #include <cstring>
@@ -12,19 +13,28 @@
 
 const int BACKLOG = 64;
 
-void write_all(int fd, const char * buf, int count)
+int safe_open(const char * path, int flags)
+{
+    int fd = open(path, flags);
+
+    if (fd == -1)
+    {
+        std::cerr << "IO error\n";
+        _exit(20);
+    }
+
+    return fd;
+}
+
+void write_all(int fd, char * buf, int count)
 {
     int written;
-    
-    // write() has no guarantee to write everything
+
     for (written = 0; written < count;)
     {
-        int t = write(fd, buf + written, count - written);
-        if (t == -1) // Write failed
-        {
-            _exit(4);
-        }
-        written += t;
+        int res = write(fd, buf + written, count - written);
+        if (res < 0) return;
+        written += res;
     }
 }
 
@@ -100,18 +110,66 @@ int main(int, char **)
         while (true) 
         {
             int fd = accept(sockfd, result->ai_addr, &result->ai_addrlen);
+
             if (fork())
             {
                 close(fd);
             } else
             {
-                dup2(fd, STDIN_FILENO);
-                dup2(fd, STDOUT_FILENO);
-                dup2(fd, STDERR_FILENO);
+                int master, slave;
+                char name[4096];
+                int pty_res = openpty(&master, &slave, name, NULL, NULL);
+                if (pty_res == -1)
+                {
+                    _exit(100);
+                }
 
-                write_all(fd, "Hello, world!\n", 14);       
-                close(fd);
-                _exit(0);
+                if (fork())
+                {
+                    close(slave);
+                    fcntl(master, F_SETFL, O_NONBLOCK);
+                    fcntl(fd, F_SETFL, O_NONBLOCK);
+
+                    const int BUF_SIZE = 4096;
+
+                    char * buf = (char*) malloc(BUF_SIZE);
+
+                    while (true)
+                    {
+                        int r1 = read(master, buf, BUF_SIZE);
+                        if (r1 > 0)
+                            write_all(fd, buf, r1);
+                        else if (r1 == 0)
+                            break;
+
+                        int r2 = read(fd, buf, BUF_SIZE);
+                        if (r2 > 0)
+                            write_all(master, buf, r2);
+                        else if (r2 == 0)
+                            break;
+
+                        sleep(1);
+                    }
+                    free(buf);
+                    close(master);
+                    close(fd);
+                } else
+                {
+                    close(master);
+                    close(fd);
+                    setsid();
+
+                    int pty_fd = safe_open(name, O_RDWR);
+                    close(pty_fd);
+
+                    dup2(slave, STDIN_FILENO);
+                    dup2(slave, STDOUT_FILENO);
+                    dup2(slave, STDERR_FILENO);
+
+                    execlp("/bin/sh", "sh", NULL);
+                    close(slave);
+                    _exit(6);
+                }
             }
         }
         
