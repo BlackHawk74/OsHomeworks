e0cc5466f31412845bb8e91a12a52485d390257d
Dmitry Bobrov <me@dbobrov.com>
2013-05-24 17:57:13 +0400

    Daemonizing

--- rshell/rshell.cpp
+++ rshell/rshell.cpp
@@ -4,6 +4,7 @@
 #include <cstring>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/wait.h>
 #include <cstdio>
 #include <netdb.h>
 #include <fcntl.h>
@@ -27,76 +28,93 @@ void write_all(int fd, const char * buf, int count)
     }
 }
 
+pid_t pid;
+
+void handler(int) {
+    kill(pid, SIGINT);
+}
+
+
 int main(int, char **)
 {
-    addrinfo hints;
-    addrinfo *result;
-
-    memset(&hints, 0, sizeof(addrinfo));
-
-    hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
-    hints.ai_socktype = SOCK_STREAM; /* Datagram socket */
-    hints.ai_flags = AI_PASSIVE;    /* For wildcard IP address */
-    hints.ai_protocol = 0;          /* Any protocol */
-
+    pid = fork();
+    if (pid)
     {
-        int ainfo = getaddrinfo(NULL, "8822", &hints, &result);
-        if (ainfo != 0)
+        signal(SIGINT, handler);
+        std::cout << "Started daemon, pid: " << pid << std::endl;
+        int status;
+        waitpid(pid, &status, 0);
+        std::cout << "Daemon stopped" << std::endl;
+    } else 
+    {
+        close(STDIN_FILENO);
+        close(STDOUT_FILENO);
+        close(STDERR_FILENO);
+        setsid();
+
+        addrinfo hints;
+        addrinfo *result;
+
+        memset(&hints, 0, sizeof(addrinfo));
+
+        hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
+        hints.ai_socktype = SOCK_STREAM; /* Datagram socket */
+        hints.ai_flags = AI_PASSIVE;    /* For wildcard IP address */
+        hints.ai_protocol = 0;          /* Any protocol */
+
         {
-            std::cerr << "Error in getaddrinfo()" << std::endl;
+            int ainfo = getaddrinfo(NULL, "8822", &hints, &result);
+            if (ainfo != 0)
+            {
+                _exit(EXIT_FAILURE);
+            }
+        }
+
+        int sockfd = socket(result->ai_family, result->ai_socktype,
+                               result->ai_protocol);
+
+        if (sockfd == -1)
+        {
             _exit(EXIT_FAILURE);
         }
-    }
 
-    int sockfd = socket(result->ai_family, result->ai_socktype,
-                           result->ai_protocol);
-
-    if (sockfd == -1)
-    {
-        std::cerr << "Error opening socket\n";
-        _exit(EXIT_FAILURE);
-    }
-
-    {
-        int optval = 1;
-        int ss = setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(int));
-        if (ss != 0)
         {
-            std::cerr << "Socket options not set\n";
+            int optval = 1;
+            int ss = setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(int));
+            if (ss != 0)
+            {
+                _exit(EXIT_FAILURE);
+            }
+        }
+
+        if (bind(sockfd, result->ai_addr, result->ai_addrlen))
+        {
             _exit(EXIT_FAILURE);
         }
-    }
 
-    if (bind(sockfd, result->ai_addr, result->ai_addrlen))
-    {
-        perror("qq");
-        std::cerr << "Could not bind socket\n";
-        _exit(EXIT_FAILURE);
-    }
-
-    if (listen(sockfd, BACKLOG))
-    {
-        std::cerr << "Could not start listening\n";
-        _exit(EXIT_FAILURE);
-    }
-
-    while (true) 
-    {
-        int fd = accept(sockfd, result->ai_addr, &result->ai_addrlen);
-        if (fork())
+        if (listen(sockfd, BACKLOG))
         {
-            close(fd);
-        } else
-        {
-            dup2(fd, STDIN_FILENO);
-            dup2(fd, STDOUT_FILENO);
-            dup2(fd, STDERR_FILENO);
-
-            write_all(fd, "Hello, world!\n", 14);       
-            close(fd);
-            _exit(0);
+            _exit(EXIT_FAILURE);
         }
+
+        while (true) 
+        {
+            int fd = accept(sockfd, result->ai_addr, &result->ai_addrlen);
+            if (fork())
+            {
+                close(fd);
+            } else
+            {
+                dup2(fd, STDIN_FILENO);
+                dup2(fd, STDOUT_FILENO);
+                dup2(fd, STDERR_FILENO);
+
+                write_all(fd, "Hello, world!\n", 14);       
+                close(fd);
+                _exit(0);
+            }
+        }
+        
     }
-    
 }
 
