386114e642f7a4a3e6115c182f70bc5f43067b1c
Dmitry Bobrov <me@dbobrov.com>
2013-05-24 17:35:13 +0400

    First version, printing hello world.

--- rshell/rshell.cpp
+++ rshell/rshell.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include <cstdlib>
 #include <cstring>
 #include <sys/types.h>
@@ -8,6 +9,24 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+const int BACKLOG = 64;
+
+void write_all(int fd, const char * buf, int count)
+{
+    int written;
+    
+    // write() has no guarantee to write everything
+    for (written = 0; written < count;)
+    {
+        int t = write(fd, buf + written, count - written);
+        if (t == -1) // Write failed
+        {
+            _exit(4);
+        }
+        written += t;
+    }
+}
+
 int main(int, char **)
 {
     addrinfo hints;
@@ -16,12 +35,9 @@ int main(int, char **)
     memset(&hints, 0, sizeof(addrinfo));
 
     hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
-    hints.ai_socktype = SOCK_DGRAM; /* Datagram socket */
+    hints.ai_socktype = SOCK_STREAM; /* Datagram socket */
     hints.ai_flags = AI_PASSIVE;    /* For wildcard IP address */
     hints.ai_protocol = 0;          /* Any protocol */
-    hints.ai_canonname = NULL;
-    hints.ai_addr = NULL;
-    hints.ai_next = NULL;
 
     {
         int ainfo = getaddrinfo(NULL, "8822", &hints, &result);
@@ -43,7 +59,7 @@ int main(int, char **)
 
     {
         int optval = 1;
-        int ss = setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
+        int ss = setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(int));
         if (ss != 0)
         {
             std::cerr << "Socket options not set\n";
@@ -51,12 +67,36 @@ int main(int, char **)
         }
     }
 
-    if (!bind(sockfd, result->ai_addr, result->ai_addrlen))
+    if (bind(sockfd, result->ai_addr, result->ai_addrlen))
     {
+        perror("qq");
         std::cerr << "Could not bind socket\n";
         _exit(EXIT_FAILURE);
     }
 
+    if (listen(sockfd, BACKLOG))
+    {
+        std::cerr << "Could not start listening\n";
+        _exit(EXIT_FAILURE);
+    }
 
+    while (true) 
+    {
+        int fd = accept(sockfd, result->ai_addr, &result->ai_addrlen);
+        if (fork())
+        {
+            close(fd);
+        } else
+        {
+            dup2(fd, STDIN_FILENO);
+            dup2(fd, STDOUT_FILENO);
+            dup2(fd, STDERR_FILENO);
+
+            write_all(fd, "Hello, world!\n", 14);       
+            close(fd);
+            _exit(0);
+        }
+    }
+    
 }
 
