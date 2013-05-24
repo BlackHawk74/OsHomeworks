a4f99f493f899fc8f530e01132e1f7217a93db44
Dmitry Bobrov <me@dbobrov.com>
2013-05-24 16:42:32 +0400

    getaddrinfo() call

--- rshell/rshell.cpp
+++ rshell/rshell.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cstdlib>
+#include <cstring>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <cstdio>
@@ -8,6 +10,27 @@
 
 int main(int, char **)
 {
+    addrinfo hints;
+    addrinfo *result;
+
+    memset(&hints, 0, sizeof(addrinfo));
+
+    hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
+    hints.ai_socktype = SOCK_DGRAM; /* Datagram socket */
+    hints.ai_flags = AI_PASSIVE;    /* For wildcard IP address */
+    hints.ai_protocol = 0;          /* Any protocol */
+    hints.ai_canonname = NULL;
+    hints.ai_addr = NULL;
+    hints.ai_next = NULL;
+
+    int ainfo = getaddrinfo(NULL, "8822", &hints, &result);
+    if (ainfo != 0)
+    {
+        std::cerr << "Error in getaddrinfo()" << std::endl;
+        _exit(EXIT_FAILURE);
+    }
+
     
+
 }
 
